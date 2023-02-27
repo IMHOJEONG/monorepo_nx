ffc25c151f61f0c5bafb0b071f772cd40f5d115e
hojeong_im <hojeong_im@emusal-mbp.local>
2023-02-27 10:08:58 +0900

    [Add]: add repos

---
+++ monorepo-standalone-node-server/jest.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default {
+  displayName: 'monorepo-standalone-node-server',
+  preset: './jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/monorepo-standalone-node-server',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+};
--- readme.md
+++ readme.md
@@ -4,4 +4,8 @@
 
   - error log 확인해보니, 네트워크 오류
 
+
+- 해결 : https://www.inflearn.com/questions/779041/solved-nestjs-프로젝트-생성-실패
+
+  - npm config 설정 변경 
   
